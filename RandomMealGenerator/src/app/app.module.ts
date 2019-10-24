9f0ddf5725c35a1b4aafe10e642d1296210253e6
Pavel <pahan.rolich@mail.ru>
2019-10-24 13:55:27 +0300

    create recipe component

--- RandomMealGenerator/src/app/app.module.ts
+++ RandomMealGenerator/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
